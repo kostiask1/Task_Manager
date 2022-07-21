921876a47772cee725bcedfb771df09f7bdd3ad6
Костя <kostia.freelancer@gmail.com>
2022-07-21 22:01:07 +0300

    local env's to typescript

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,5 +3,12 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test"
     PUBLIC_URL: string
+    REACT_APP_API_KEY: string
+    REACT_APP_AUTH_DOMAIN: string
+    REACT_APP_PROJECT_ID: string
+    REACT_APP_STORAGE_BUCKET: string
+    REACT_APP_MESSAGING_SENDER_ID: string
+    REACT_APP_APP_ID: string
+    REACT_APP_MEASUREMENT_ID: string
   }
 }
