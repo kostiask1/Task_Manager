b5bc40efe9914b52432ef4845e057f5519d4b363
Костя <kostia.freelancer@gmail.com>
2022-07-25 10:26:56 +0300

    block update user profile if data is the same

--- src/pages/Profile/General/General.tsx
+++ src/pages/Profile/General/General.tsx
@@ -20,40 +20,34 @@ import { setUser } from "../../../store/authSlice"
 import { RootState, useAppDispatch, useAppSelector } from "../../../store/store"
 import { User } from "../../../store/types"
 import "./General.scss"
+import { equal } from "../../../helpers"
 
 const General = () => {
   const dispatch = useAppDispatch()
-  const user: User | null = useAppSelector(
-    (state: RootState) => state.auth.user
-  )
-  const [firstName, setFirstName] = useState(user?.firstName || "")
-  const [lastName, setLastName] = useState(user?.lastName || "")
-  const [email, setEmail] = useState(user?.email || "")
-  const [profileImg, setProfileImg] = useState(user?.profileImg || "")
+  const user: User = useAppSelector((state: RootState) => state.auth.user)
+  const [userData, setUserData] = useState<User>(user)
   const [files, setFiles] = useState<FileList>()
   const [loading, setLoading] = useState(false)
-  const id = user?.id || ""
-  const admin = user?.admin || false
   const imageRef = useRef<HTMLImageElement>(null)
 
   const submitHandler = (e: FormEvent) => {
     e.preventDefault()
 
-    if (!files && !profileImg.includes("https")) {
+    if (!files && !userData.profileImg.includes("https")) {
       return dispatch(setError("Profile image is not a link"))
     }
     if (user) {
       setLoading(true)
       if (files) {
         deleteImage(user.profileImg)
-        uploadImage(files, id + new Date().getTime(), "users").then(
+        uploadImage(files, user.id + new Date().getTime(), "users").then(
           (imageUrl: any) => {
             dispatch(setSuccess("Image updated successfully"))
-            setProfileImg(imageUrl)
+            setProfileImg(null, imageUrl)
             updateUserProfile(imageUrl)
           }
         )
-      } else if (user?.profileImg !== profileImg) {
+      } else if (user?.profileImg !== userData.profileImg) {
         deleteImage(user.profileImg)
         updateUserProfile()
       } else {
@@ -65,36 +59,34 @@ const General = () => {
   const updateUserProfile = async (image?: string) => {
     const auth = getAuth()
     if (auth.currentUser) {
-      const userData: User = {
-        email,
-        firstName,
-        lastName,
-        id,
-        admin,
-        profileImg: image || profileImg,
-        password: user.password,
+      const updatedProfile: User = {
+        ...userData,
+        profileImg: image || userData.profileImg,
         emailVerified:
-          user.email !== email ? false : auth.currentUser.emailVerified,
+          user.email !== userData.email ? false : user.emailVerified,
       }
       updateProfile(auth.currentUser, {
-        displayName: firstName,
-        photoURL: profileImg,
+        displayName: updatedProfile.firstName,
+        photoURL: updatedProfile.profileImg,
       })
-      await uploadDoc("users", userData)
-      dispatch(setUser(userData))
+      await uploadDoc("users", updatedProfile)
+      dispatch(setUser(updatedProfile))
       dispatch(setSuccess("Profile updated successfully"))
       setLoading(false)
-      if (user.email !== email) {
+      if (user.email !== updatedProfile.email) {
         const credential = EmailAuthProvider.credential(
           user.email,
           user.password
         )
         reauthenticateWithCredential(auth.currentUser, credential).then(() => {
-          updateEmail(auth.currentUser as any, email).then(() => {
-            sendEmailVerification(auth.currentUser as any).then(() =>
-              dispatch(setError("Check your email to verify your account!"))
-            )
-          })
+          updateEmail(auth.currentUser as any, updatedProfile.email).then(
+            () => {
+              sendEmailVerification(auth.currentUser as any).then(() => {
+                setUserData((state) => ({ ...state, emailVerified: false }))
+                dispatch(setError("Check your email to verify your account!"))
+              })
+            }
+          )
         })
       }
     } else {
@@ -105,12 +97,32 @@ const General = () => {
   useEffect(() => {
     if (files && Array.from(files)?.length) {
       const url = URL.createObjectURL(files[0])
-      setProfileImg("")
+      setProfileImg()
       setTimeout(() => {
         if (imageRef.current) imageRef.current.src = url
       })
     }
   }, [files])
+
+  const setFirstName = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUserData((state): User => ({ ...state, firstName: e.target.value }))
+
+  const setLastName = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUserData((state): User => ({ ...state, lastName: e.target.value }))
+
+  const setProfileImg = (
+    e?: React.ChangeEvent<HTMLInputElement> | null,
+    value?: string
+  ) =>
+    setUserData(
+      (state): User => ({
+        ...state,
+        profileImg: e?.target.value || value || "",
+      })
+    )
+
+  const setEmail = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUserData((state): User => ({ ...state, email: e.target.value }))
 
   const uploadProfileImg = (e: React.ChangeEvent<HTMLInputElement>) =>
     e.target.files && setFiles(e.target.files)
@@ -123,7 +135,8 @@ const General = () => {
             ref={imageRef}
             className="is-rounded"
             src={
-              profileImg || "https://bulma.io/images/placeholders/128x128.png"
+              userData.profileImg ||
+              "https://bulma.io/images/placeholders/128x128.png"
             }
             style={{ maxHeight: "128px" }}
             onError={({ currentTarget }) => {
@@ -141,8 +154,8 @@ const General = () => {
           name="firstName"
           minLength={2}
           maxLength={20}
-          value={firstName}
-          onChange={(e) => setFirstName(e.currentTarget.value)}
+          value={userData.firstName}
+          onChange={(e) => setFirstName(e)}
           placeholder="set Name"
           label="Name"
           required
@@ -152,8 +165,8 @@ const General = () => {
           name="lastName"
           minLength={2}
           maxLength={30}
-          value={lastName}
-          onChange={(e) => setLastName(e.currentTarget.value)}
+          value={userData.lastName}
+          onChange={(e) => setLastName(e)}
           placeholder="set Surname"
           label="Surname"
           required
@@ -162,8 +175,8 @@ const General = () => {
           type="text"
           name="profileImg"
           minLength={10}
-          value={profileImg}
-          onChange={(e) => setProfileImg(e.currentTarget.value)}
+          value={userData.profileImg}
+          onChange={(e) => setProfileImg(e)}
           placeholder="set Profile Image"
           label="Profile Image"
           required={!files}
@@ -179,10 +192,10 @@ const General = () => {
         <Input
           type="email"
           name="in_email"
-          value={email}
+          value={userData.email}
           minLength={6}
           maxLength={40}
-          onChange={(e) => setEmail(e.currentTarget.value)}
+          onChange={(e) => setEmail(e)}
           placeholder="Email address"
           label="Email address"
           required
@@ -190,12 +203,12 @@ const General = () => {
         <Checkbox
           text="Email verificated"
           disabled={true}
-          checked={user.emailVerified}
+          checked={userData.emailVerified}
         />
         <Button
           text={loading ? "Loading..." : "Update Profile"}
           className="is-primary is-fullwidth mt-5"
-          disabled={loading}
+          disabled={equal(user, userData) || loading}
         />
       </form>
     </div>
