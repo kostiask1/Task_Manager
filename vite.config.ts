e208ada3d6825c166d4d060479e6ef245afa5f4b
Костя <kostia.freelancer@gmail.com>
2022-07-25 17:29:08 +0300

    remove deprecated plugin comeback variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import reactRefresh from "@vitejs/plugin-react-refresh"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [react(), reactRefresh()],
+  plugins: [react()],
 })
