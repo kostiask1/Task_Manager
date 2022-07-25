0ed4f5cf416a4f767c9e3ceb99b33d229b8033b7
Костя <kostia.freelancer@gmail.com>
2022-07-25 19:20:10 +0300

    vite add typescript linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import Checker from "vite-plugin-checker"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [react()],
+  plugins: [Checker({ typescript: true }), react()],
 })
