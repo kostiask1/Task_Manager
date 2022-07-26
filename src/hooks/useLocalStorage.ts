4c8352af5b3308c34a6baea419373915f4642973
Костя <kostia.freelancer@gmail.com>
2022-07-26 11:25:50 +0300

    new localstorage hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,21 @@
+import { useState, useCallback, useEffect } from "react"
+
+export function useLocalStorage(
+  key: string,
+  initialValue: any
+): [any, (value: any) => void] {
+  const [value, setValue] = useState(() => {
+    const item = localStorage.getItem(key)
+    return item ? JSON.parse(item) : initialValue
+  })
+  const setLocalStorage = useCallback(
+    (value: any) => {
+      localStorage.setItem(key, JSON.stringify(value))
+    },
+    [key]
+  )
+  useEffect(() => {
+    setLocalStorage(value)
+  }, [setLocalStorage, value])
+  return [value, setValue]
+}
