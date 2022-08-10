a8bb1e5041d560ca3e5422e04c8dacf4659b7931
Костя <kostia.freelancer@gmail.com>
2022-08-10 14:32:31 +0300

    added btn to share your list

--- src/pages/Wishlist/Wishlist.tsx
+++ src/pages/Wishlist/Wishlist.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect, useState } from "react"
+import { lazy, Suspense, useEffect, useState, useCallback } from "react"
 import { useParams } from "react-router-dom"
 import Loader from "../../components/UI/Loader/Loader"
 import { User } from "../../store/Auth/types"
@@ -7,11 +7,12 @@ import { getWishes } from "../../store/Wish/slice"
 import { Wish as IWish } from "../../store/Wish/types"
 import WishForm from "./WishForm/WishForm"
 import "./Wishlist.scss"
+import { setSuccess, setError } from "../../store/App/slice"
+import Button from "../../components/UI/Button"
 const Wish = lazy(() => import("./Wish"))
 
 const Wishlist = () => {
   const dispatch = useAppDispatch()
-
   const wishes: IWish[] = useAppSelector(
     (state: RootState) => state.wishes.array
   )
@@ -28,8 +29,25 @@ const Wishlist = () => {
     dispatch(getWishes(uid || user.id)).then(() => setLoading(false))
   }, [uid])
 
+  const copyWishPage = useCallback(() => {
+    navigator.clipboard
+      .writeText(`${window.location.host}/wishes/${user.id}`)
+      .then(
+        () =>
+          dispatch(setSuccess("Link to your wishlist is copied to clipboard")),
+        () => dispatch(setError("Something went wrong"))
+      )
+  }, [user.id])
+
   return (
     <div className="pb-6">
+      {!uid && (
+        <Button
+          onClick={copyWishPage}
+          className="is-primary mb-3"
+          text="Share Your Wishlist"
+        />
+      )}
       {!uid && <WishForm key={JSON.stringify(wish)} />}
       {uid && <h2>Tasks of user ID: {uid}</h2>}
       <hr />
