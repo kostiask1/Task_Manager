cb9b2f7942651d614c7dbd8ecf10683202a421c7
Костя <kostia.freelancer@gmail.com>
2022-08-10 11:13:24 +0300

    wishlist base component

--- src/pages/Wishlist/Wishlist.tsx
+++ src/pages/Wishlist/Wishlist.tsx
@@ -1,11 +1,36 @@
-import "./Wishlist.scss";
+import { Suspense, useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import Loader from "../../components/UI/Loader/Loader"
+import { User } from "../../store/Auth/types"
+import { RootState, useAppDispatch, useAppSelector } from "../../store/store"
+import { getWishes } from "../../store/Wish/slice"
+import WishForm from "./WishForm/WishForm"
+import "./Wishlist.scss"
 
 const Wishlist = () => {
+  const dispatch = useAppDispatch()
+
+  const location = useLocation()
+  const wishes = useAppSelector((state: RootState) => state.wishes.array)
+  const user: User = useAppSelector((state: RootState) => state.auth.user)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(!wishes.length)
+    dispatch(getWishes(user.id)).then(() => setLoading(false))
+  }, [])
+
+  console.log("wishes:", wishes)
+  console.log("location:", location)
   return (
     <>
-      <div />
-    </>
-  );
-};
+      <WishForm />
 
-export default Wishlist;
+      <Suspense fallback={<Loader loading={true} />}>
+        <Loader loading={loading} />
+      </Suspense>
+    </>
+  )
+}
+
+export default Wishlist
