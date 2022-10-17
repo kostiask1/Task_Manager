c0a314bc278350a7c151b1837033f0d5b14e4548
Костя <kostia.freelancer@gmail.com>
2022-10-17 21:52:34 +0300

    new Table component

---
+++ src/components/UI/Table.tsx
@@ -0,0 +1,76 @@
+import { equal } from '../../helpers';
+import Button from './Button';
+
+type IHeader = {
+  data: string,
+  element?: React.ReactElement,
+  sorting?: boolean
+} | {
+  title: string | number,
+  element?: React.ReactElement,
+  sorting?: boolean
+}
+
+type IFooter = {
+  value: string | number
+} | {
+  element: React.ReactElement
+}
+
+export interface ITableProps<T = unknown> {
+  columns: IHeader[],
+  data: T[],
+  initData: T[],
+  body: React.ReactElement[],
+  footer: (IFooter | string)[],
+  loading: boolean,
+  sorting: string,
+  sort: (e: React.MouseEvent<HTMLElement>, key?: string) => void,
+  reset: (e: React.MouseEvent<HTMLElement>, key?: string) => void
+}
+
+const Table: <T>(p: ITableProps<T>) => React.ReactElement = ({ columns, data, initData, body, footer, loading, sorting, sort, reset }) => {
+  return <div className="table-container">
+    <table className="table table-debts is-striped is-bordered is-hoverable is-fullwidth is-narrow">
+      <thead>
+        <tr>
+          <th>
+            <Button
+              text="Reset"
+              onClick={reset}
+              className={
+                !loading && (!equal(initData, data) || sorting)
+                  ? "is-danger"
+                  : ""
+              }
+              disabled={equal(initData, data) && !sorting}
+            />
+          </th>
+          {columns.map(({ title, data, element, sorting = true }: any, index: number) => <th key={title + index} onClick={(e) => sorting && sort(e, data || title.toLowerCase())}>{element || title}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {!!body?.length ? (
+          body.map((element: any, index: number) => (
+            <tr key={(element.id || 0) + index}>
+              {element}
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={10}>No debts to be displayed...</td>
+          </tr>
+        )}
+      </tbody>
+      {data?.length > 1 && (
+        <tfoot>
+          <tr>
+            {footer.map(({ value = "", element }: any, index: number) => <th key={(value || 0) + index}>{element || value}</th>)}
+          </tr>
+        </tfoot>
+      )}
+    </table>
+  </div>
+}
+
+export default Table
