1351f7d0bd2076b0161bea091f3932d1b57af313
Костя <kostia.freelancer@gmail.com>
2022-10-26 21:24:52 +0300

    send modal to top layer

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react"
+import { createPortal } from "react-dom"
 import "./Modal.scss"
 
 interface Props {
@@ -8,15 +9,15 @@ interface Props {
   hide: () => void
 }
 
-const Modal: FC<Props> = ({ show, hide, id, children }) => show ? (
-  <div
-    id={id}
-    className={`modal fadeIn ${show ? "is-active" : ""}`}
-    aria-label="modal"
-  >
-    <div className="modal-background" onClick={hide}></div>
-    <div className="modal-content">{children}</div>
-  </div>
-) : null
+const Modal: FC<Props> = ({ show, hide, id, children }) => show ? createPortal(<div
+  id={id}
+  className={`modal fadeIn ${show ? "is-active" : ""}`}
+  aria-label="modal"
+>
+  <div className="modal-background" onClick={hide}></div>
+  <div className="modal-content">{children}</div>
+</div>, document.getElementById("modal") as HTMLElement ) : null
+
+
 
 export default Modal
